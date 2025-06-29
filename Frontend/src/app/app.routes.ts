568f4512b93a9dcdd0b4d059e37502db4a1a4a2f
Jaider <jaiderandres.garciagarcia@gmail.com>
2025-06-29 11:20:48 -0500

    Frontend Upload

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,30 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import {Routes} from '@angular/router';
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./shared/components/layout/layout.js').then(m => m.Layout),
+        children: [
+            {
+                path: 'dashboard',
+                loadComponent: () => import('./business/dashboard/dashboard.js').then(m => m.Dashboard)
+            },
+            {
+                path: 'profile',
+                loadComponent: () => import('./business/profile/profile.js').then(m => m.Profile)
+            },
+            {
+                path: 'tables',
+                loadComponent: () => import('./business/tables/tables.js').then(m => m.Tables)
+            },
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            }
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard'
+    }
+];
